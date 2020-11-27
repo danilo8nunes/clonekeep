dda7bf9fd4b6cf359b10c1319b9c7acd9d8a3b7a
Danilo Nunes <danilo8nunes@gmail.com>
2020-11-26 22:53:37 -0300

    Início da criação do template principal

---
+++ _template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
+    <title>Keep Clone</title>
+<body>
+    <header class="content">
+        <div class="content_logo-menu">
+            <div class="content_logo-menu_icon">
+                <i class="fas fa-bars"></i>
+            </div>
+            <div class="content_logo-menu_name">Keep</div>
+        </div>
+        <div class="content_search">
+            <form method="get" action="#" enctype="multipart/form-data">
+                <span>
+                    <i class="fas fa-search"></i>
+                </span>
+                <input type="text" name="search" placeholder="Pesquisar">
+            </form>
+        </div>
+        <div class="content_icons-right">
+            <i class="fas fa-redo-alt"></i>
+            <i class="fas fa-cog"></i>
+            <i class="far fa-user"></i>
+        </div>
+    </header>
+    
+    <script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
+</body>
+</html>
