a147859af8c7b29f95f8366f8781c796df3799ba
Danilo Nunes <danilo8nunes@gmail.com>
2020-12-06 21:34:23 -0300

    Correções e resposiviade do menu

--- _template.php
+++ _template.php
@@ -7,28 +7,38 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
     <title>Keep Clone</title>
 <body>
-    <header class="content">
-        <div class="content_logo-menu">
-            <div class="content_logo-menu_icon">
-                <i class="fas fa-bars"></i>
-            </div>
-            <div class="content_logo-menu_name">Keep</div>
-        </div>
-        <div class="content_search">
-            <form method="get" action="#" enctype="multipart/form-data">
-                <span>
-                    <i class="fas fa-search"></i>
-                </span>
-                <input type="text" name="search" placeholder="Pesquisar">
-            </form>
-        </div>
-        <div class="content_icons-right">
-            <i class="fas fa-redo-alt"></i>
-            <i class="fas fa-cog"></i>
-            <i class="far fa-user"></i>
-        </div>
-    </header>
-    
+    <section class="section_nav">
+        <header class="content">
+            <nav class="nav_content">
+                <div class="nav_content_menu">
+                    <i class="fas fa-bars icon_menu"></i>
+                    <span class="nav_menu_name">Keep</span>
+                </div>
+                <form class="nav_content_form" method="get" action="#" enctype="multipart/form-data">
+                    <span class="nav_content_form_pre-input">
+                        <i class="fas fa-search icon_menu"></i>
+                    </span>
+                    
+                    <input class="nav_content_form_input" type="text" name="search" placeholder="Pesquisar">
+                    <span class="nav_content_form_pos-input">
+                        <i class="fas fa-times icon_menu"></i>
+                    </span>
+                    <span class="nav_content_form_for-media">
+                        <i class="fas fa-search icon_menu"></i>
+                    </span>
+                </form>
+                <div class="nav_content_perfil">
+                    <i class="fas fa-redo-alt icon_menu"></i>
+                    <i class="fas fa-cog icon_menu"></i>
+                    <i class="far fa-user icon_menu"></i>
+                </div>
+            </nav>
+        </header>
+    </section>
+    <section>
+
+    </section>
+  
     <script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
 </body>
 </html>
