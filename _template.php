05bfd0b9259fa5ec34f554a6828ae1f92d621eec
Danilo Nunes <danilo8nunes@gmail.com>
2020-12-07 22:45:40 -0300

    Criação do sidebar

--- _template.php
+++ _template.php
@@ -35,10 +35,32 @@
             </nav>
         </header>
     </section>
-    <section>
-
+    <section class="section_sidebar">
+        <article class="content_sidebar">
+            <nav class="content_sidebar_menu">
+                <ul class="content_sidebar_list">
+                    <li class="content_sidebar_item item_active">
+                        <a href="#" title="Item 1"><i class="far fa-lightbulb icon_sidebar"></i></i>Notas</a></li>
+                    <li class="content_sidebar_item">
+                        <a href="#" title="Item 1"><i class="fas fa-bookmark icon_sidebar"></i>Item 1</a></li>
+                    <li class="content_sidebar_item">
+                        <a href="#" title="Item 1"><i class="fas fa-bookmark icon_sidebar"></i>Item 2</a></li>
+                    <li class="content_sidebar_item">
+                        <a href="#" title="Item 1"><i class="fas fa-bookmark icon_sidebar"></i>Item 3</a></li>
+                    <li class="content_sidebar_item">
+                        <a href="#" title="Item 1"><i class="fas fa-bookmark icon_sidebar"></i>Item 4</a></li>
+                    <li class="content_sidebar_item">
+                        <a href="#" title="Item 1"><i class="fas fa-bookmark icon_sidebar"></i>Item 5</a></li>             
+                    <li class="content_sidebar_item">
+                        <a href="#" title="Item 1"><i class="fas fa-pen icon_sidebar"></i>Editar</a></li>             
+                    <li class="content_sidebar_item">
+                        <a href="#" title="Item 1"><i class="fas fa-folder icon_sidebar"></i>Arquivo</a></li>             
+                    <li class="content_sidebar_item">
+                        <a href="#" title="Item 1"><i class="fas fa-trash icon_sidebar"></i>Lixeira</a></li>             
+                </ul>
+            </nav>
+        </article>
     </section>
-  
     <script type="text/javascript" src="assets/js/jquery-3.5.1.min.js"></script>
 </body>
 </html>
